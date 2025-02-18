bf7bdfb5c509aec7c2a3ee733f547dcddd6f4ae8
P-Sirisha-11 <P.Sirisha@ibm.com>
2025-02-18 15:01:13 +0530

    Add files via upload

---
+++ Python/Activities/Activity15.py
@@ -0,0 +1,5 @@
+
+try:
+    print(num)
+except NameError:
+    print("Name Error exception is thrown, num is not defined")
---
+++ Python/Activities/Activity17.py
@@ -0,0 +1,13 @@
+
+import pandas as pd
+
+credentials = {
+    "Usernames": ["admin","Charles","Deku"],
+    "Passwords": ["password","Charl13","AllMight"]
+}
+
+dataFrame = pd.DataFrame(credentials)
+
+print(dataFrame)
+
+dataFrame.to_csv("credentials.csv",index=False)
