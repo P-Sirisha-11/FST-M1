996fb4064eaf8b82c309e16db2cdff384364a2e6
P-Sirisha-11 <P.Sirisha@ibm.com>
2025-02-18 15:29:02 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity8.java
@@ -0,0 +1,35 @@
+
+
+public class Activity8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		<?xml version="1.0" encoding="UTF-8" ?>
+		<!DOCTYPE suite SYSTEM "https://testng.org/testng-1.0.dtd" >
+		<suite name="FSTTests" thread-count="4" parallel="classes">
+		    <!-- Comment out groups to run everything -->
+			<groups>
+		        <run>
+		            <include name="HeaderTests" />
+		        </run>
+		    </groups>
+		    <parameter name="username" value="admin" />
+		    <parameter name="password" value="password" />
+		    <test name="FSTActivities">
+		        <classes>
+		            <class name="activities.Activity1" />
+		            <class name="activities.Activity2" />
+		            <class name="activities.Activity3" />
+		            <class name="activities.Activity4_DemoOne" />
+		            <class name="activities.Activity4_DemoTwo" />
+		            <class name="activities.Activity5" />
+		            <class name="activities.Activity6" />
+		            <class name="activities.Activity7" />
+		        </classes>
+		    </test>
+		</suite>
+		
+	}
+
+}
